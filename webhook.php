f963f4a6ef6c8fa2380f9eb5994539d1105ad1d3
Yasin <65618247+suphiyasin@users.noreply.github.com>
2024-02-15 23:06:12 +0300

    Added username log

--- webhook.php
+++ webhook.php
@@ -1,12 +1,25 @@
 <?php
 include("api.php");
 $a = new downloadgram();
-$a->BotToken = 'YOUR TOKEN';
+$botToken = 'YOUR TOKEN';
+$a->BotToken = $botToken;
 $update = json_decode(file_get_contents("php://input"), TRUE);
 
 if (isset($update["message"])) {
     $chatId = $update["message"]["chat"]["id"];
     $message = $update["message"]["text"];
+    $userInfo = json_decode(file_get_contents("https://api.telegram.org/bot$botToken/getChat?chat_id=$chatId"), true);
+
+    if ($userInfo && isset($userInfo['ok']) && $userInfo['ok']) {
+        $username = $userInfo['result']['username'];
+        $first_name = $userInfo['result']['first_name'];
+        if(isset($username) and $username !== null){
+            $a->username = $username;
+        }else{
+             $a->username = $first_name; 
+        }
+      
+    }
     $a->chatid = $chatId;
     echo $a->getCheck($message);
 } else {
